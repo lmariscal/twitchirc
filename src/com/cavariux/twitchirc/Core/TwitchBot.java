f6dc418010e3fda78c3a29f0e1aee6ca17c76618
Leonardo David Mariscal Delgado <ldavid.mariscal@hotmail.com>
2016-01-23 15:01:58 -0600

    After all this months, I decided to research more in the twitch api and irc. So I added new feathres
    ++Added | --Removed | *Extra
    -----------------------------------
    ++ Added the Join and Part lister in the TwitchBot
    ++ Added new verbose features from twitch
    * Working on whispers, will probably take a while

--- src/com/cavariux/twitchirc/Core/TwitchBot.java
+++ src/com/cavariux/twitchirc/Core/TwitchBot.java
@@ -18,7 +18,11 @@ import com.cavariux.twitchirc.Chat.User;
  */
 public class TwitchBot {
 	
+	private String whispers_ip = "";
+	private int whispers_port = 443;
+	private boolean wen = true;
 	private String user;
+	private boolean started = false;
 	private String oauth_key;
 	private BufferedWriter writer;
 	private BufferedReader reader;
@@ -28,10 +32,21 @@ public class TwitchBot {
 	public TwitchBot(){}
 	
 	/**
-	 * The connect method alouds you to connect to the IRC servers on twitch
+	 * Here you can connect without having to connect to a chat group
 	 */
-	public void connect()
+	public void connect() {
+		wen = false;
+		connect("irc.twitch.tv", 6667);
+	}
+	
+	/**
+	 * The connect method alouds you to connect to the IRC servers on twitch
+	 * @param ip The ip of the Chat group
+	 * @param port The port of the Chat group
+	 */
+	public void connect(String ip, int port)
 	{
+		if (started) return;
 		try{
 			if (user == null || user == "")
 			{
@@ -46,15 +61,16 @@ public class TwitchBot {
 				return;
 			}
 			
-			
 			@SuppressWarnings("resource")
-			Socket socket = new Socket("irc.twitch.tv", 6667);
+			Socket socket = new Socket(ip, port);
 			this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
 			this.writer.write("PASS " + oauth_key + "\r\n");
 			this.writer.write("NICK " + user + "\r\n");
 			this.writer.write("USER " + this.getVersion() + " \r\n");
+			this.writer.write("CAP REQ :twitch.tv/commands \r\n");
+			this.writer.write("CAP REQ :twitch.tv/membership \r\n");
 			this.writer.flush();
 			
 			String line = "";
@@ -233,6 +249,7 @@ public class TwitchBot {
 	 */
 	public final void start()
 	{
+		if (started) return;
 		String line = "";
 		try {
 			while ((line = this.reader.readLine( )) != null) {
@@ -255,7 +272,41 @@ public class TwitchBot {
 			        	onCommand(msg_user, msg_channel, msg_msg.substring(1));
 			        
 			        onMessage(msg_user, msg_channel, msg_msg);
-			    } else if (line.toLowerCase().contains("disconnected"))
+			    } else if (line.contains(" JOIN ")) {
+			    	String[] p = line.split(" ");
+			    	String[] pd = line.split("!");
+			    	if (p[1].equals("JOIN")) 
+			    		userJoins(User.getUser(pd[0].substring(1)), Channel.getChannel(p[2], this));
+				} else if (line.contains(" PART ")) {
+			    	String[] p = line.split(" ");
+			    	String[] pd = line.split("!");
+			    	if (p[1].equals("PART")) 
+			    		userParts(User.getUser(pd[0].substring(1)), Channel.getChannel(p[2], this));
+				} else if (line.startsWith(":tmi.twitch.tv ROOMSTATE")) {
+			    	
+				} else if (line.startsWith(":tmi.twitch.tv NOTICE"))
+			    {
+			    	String[] parts = line.split(" ");
+			    	if (line.contains("This room is now in slow mode. You may send messages every"))
+			    	{
+			    		System.out.println("> Chat is now in slow mode. You can send messages every " + parts[15] + " sec(s)!");
+			    	} else if (line.contains("subscribers-only mode")) {
+			    		if (line.contains("This room is no longer"))
+			    			System.out.println("> The room is no longer Subscribers Only!");
+			    		else
+			    			System.out.println("> The room has been set to Subscribers Only!");
+			    	} else {
+			    		System.out.println(line);
+			    	}
+			    } else if (line.startsWith(":jtv MODE "))
+			    {
+			    	String[] p = line.split(" ");
+			    	if (p[3].equals("+o")) {
+			    		System.out.println("> +o " + p[4]);
+			    	} else {
+			    		System.out.println("> -o " + p[4]);
+			    	}
+			    }else if (line.toLowerCase().contains("disconnected"))
 			    {
 			    	System.out.println(line);
 			    	this.connect();
@@ -267,6 +318,75 @@ public class TwitchBot {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		started = true;
+	}
+	
+	/**
+	 * A user joins the channel
+	 * @param user The user that has join
+	 * @param channel The channel he joined
+	 */
+	protected void userJoins(User user, Channel channel)
+	{
+		
+	}
+	
+	/**
+	 * A user leaves/parts the channel
+	 * @param user The user that has left
+	 * @param channel The channel he left
+	 */
+	protected void userParts(User user, Channel channel)
+	{
+		
+	}
+	
+	/**
+	 * This will let you whisper to the specified user
+	 * @param user The user to send the message
+	 * @param message The messsage to send
+	 */
+	public void whisper(User user, String message)
+	{
+		if (!wen) {
+			System.out.println("You cannot send whispers while connected to the default Twitch Server");
+		}
+		sendRawMessage("PRIVMSG #jtv :/w " + user + " " + message);
+	}
+	
+	/**
+	 * When overrided this method lets you check for whispers.
+	 * @param user The user that sent it
+	 * @param message The message he sent
+	 */
+	protected void onWhisper(User user, String message)
+	{
+	}
+	
+	/**
+	 * Sets the whispers ip (000.000.000.000:0000)
+	 * @param ip The whole ip
+	 */
+	protected final void setWhispersIp(String ip)
+	{
+		if (!ip.contains(":")) {
+			System.out.println("Invaid ip!");
+			return;
+		}
+		String[] args = ip.split(":");
+		whispers_ip = args[0];
+		whispers_port = Integer.parseInt(args[1]);
+	}
+	
+	/**
+	 * Sets the whispers ip
+	 * @param ip The ip to connect
+	 * @param port The port to connect with
+	 */
+	protected final void setWhispersIp(String ip, int port)
+	{
+		whispers_ip = ip;
+		whispers_port = port;
 	}
 	
 	/**
