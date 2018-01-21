535c01c60840e946271bce3a7d15182bc5d8afe7
geisterfurz007 <geisterfurz007@gmail.com>
2018-01-21 16:21:29 +0100

    Added required header, corrected JSON parsing

--- src/com/cavariux/twitchirc/TwitchAPIConnections/UserQueries.java
+++ src/com/cavariux/twitchirc/TwitchAPIConnections/UserQueries.java
@@ -7,6 +7,7 @@ import com.cavariux.twitchirc.Json.JsonValue;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
@@ -16,13 +17,15 @@ public class UserQueries {
 	private static final String V5_API_BASE = "https://api.twitch.tv/kraken/users";
 	
 	public static int[] getUserId(String clientId, String... usernames) {
-		String names = Arrays.toString(usernames);
+		String names = Arrays.toString(usernames).replace(" ", "");
 		
 		String requestUri = V5_API_BASE + "?login=" + names.substring(1, names.length()-1);
 		try {
 			URL request = new URL(requestUri);
-			URLConnection conn = request.openConnection();
+			HttpURLConnection conn = (HttpURLConnection) request.openConnection();
+			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Client-ID", clientId);
+			conn.setRequestProperty("Accept", "application/vnd.twitchtv.v5+json");
 			Reader response = new InputStreamReader(conn.getInputStream());
 			JsonObject obj = JsonObject.readFrom(response);
 			JsonArray users = obj.get("users").asArray();
@@ -31,12 +34,13 @@ public class UserQueries {
 			int index = 0;
 			
 			for (JsonValue user : users) {
-				int id = user.asObject().get("_id").asInt();
+				int id = Integer.parseInt(user.asObject().get("_id").asString());
 				result[index] = id;
 				index++;
 			}
 			return result;
 		} catch (IOException e) {
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 		return new int[]{0, 0};
